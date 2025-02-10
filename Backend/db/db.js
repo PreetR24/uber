faa0f7d18c2896887b4db6176ceeb7e2de133d97
Preet Rupapara <preetrupapara247@gmail.com>
2025-02-10 23:50:14 +0530

    backend

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT).then(() => {
-        console.log("Connected to DB");
-    }). catch (err =>  console.error('Error connecting to MongoDB:', err));
-};
+    mongoose.connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to DB"))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
+}
 
 module.exports = connectToDb;
