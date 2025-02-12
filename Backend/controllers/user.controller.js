e3c0308cf87f89b6f2c265a6e7e9a803c0159be7
Preet Rupapara <preetrupapara247@gmail.com>
2025-02-12 23:17:59 +0530

    fixed path

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,7 +2,7 @@ const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
 const blacklistTokenModel = require('../models/blacklistToken.model');
-const OTP = require('../models/OTP');
+const OTP = require('../models/otp');
 const { sendEmail } = require('../utils/sendEmail');
 
 module.exports.registerUser = async(req, res, next) => {
